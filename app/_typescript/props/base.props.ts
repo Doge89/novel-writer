c3ce8e0737ab6e16ea14b3778edf69c2febb60a8
Doge89 <undertale.9055@gmail.com>
2025-05-10 21:21:23 -0600

    Adding required AuthLayoutProps

--- app/_typescript/props/base.props.ts
+++ app/_typescript/props/base.props.ts
@@ -3,8 +3,8 @@ import React from 'react';
 export type NodeWithChildrenProps<TProps> = React.PropsWithChildren<TProps>;
 
 export interface AuthLayoutProps {
-    signin?: React.ReactNode;
-    register?: React.ReactNode;
+    signin: React.ReactNode;
+    register: React.ReactNode;
 }
 
 export interface ParamsProps<T> {
