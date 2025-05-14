b4df16842ca4754b98813b53e4d62e3f12fd5ded
Doge89 <undertale.9055@gmail.com>
2025-05-13 20:13:17 -0600

    Adding webpack rules to support SVG

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,29 @@ const nextConfig: NextConfig = {
         }
       }
     }
-  }
+  },
+  webpack(config, { isServer, webpack }) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: any) => rule.test && rule.test.test && rule.test.test('.svg')
+    );
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            dimensions: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
